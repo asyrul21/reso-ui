21a509767076762f9aef87b7e1c1281659832bdb
asyrul21 <asyrulhafetzy.21@gmail.com>
2024-02-24 09:58:01 +0800

    fix: minor useFormInput error type bug

--- src/library/forms/Hooks/useFormInput/index.ts
+++ src/library/forms/Hooks/useFormInput/index.ts
@@ -1,9 +1,9 @@
 import { FormEvent, useState } from "react";
-import { FormInputHookReturnObj, FormInputValidator } from "../../../interfaces/Form";
+import { FormInputHookReturnObj } from "../../../interfaces/Form";
 
 export const useFormInput = <T>(initialValue: T): FormInputHookReturnObj<T> => {
   const [inputValue, setInputValue] = useState<T>(initialValue);
-  const [inputError, setInputError] = useState<string>(null);
+  const [inputError, setInputError] = useState<string | undefined>(undefined);
 
   return {
     value: inputValue,
